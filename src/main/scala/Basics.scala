9843b03ec205707de7e4ecc24dd72e5725bc5cf3
sidendev <96108993+sidendev@users.noreply.github.com>
2025-07-05 17:11:57 +0100

    Add basic types and functions from RTJVM material

--- src/main/scala/Basics.scala
+++ src/main/scala/Basics.scala
@@ -35,4 +35,31 @@ object Basics extends App {
     aLocalValue + 3
   }
 
+  def sayHello(name: String): Unit = println(s"Hello, $name")
+  sayHello("Bertie")
+
+  // define a function - this outputs a string
+  def myFunction(x: Int, y: String): String = {
+    y + " " + x
+  }
+
+  // recursive function - avoid variables and loops
+  def factorial(n: Int): Int =
+    if (n <= 1) 1
+    else n * factorial(n - 1)
+
+//  factorial(5) = 5 * factorial(4) = 5 * 24 = 120
+//  factorial(4) = 4 * factorial(3) = 4 * 6
+//  factorial(3) = 3 * factorial(2) = 3 * 2
+//  factorial(2) = 2 * factorial(1) = 2 * 1
+//  factorial(1) = 1
+
+  // The Unit type - same as void in Java
+  // type of side effects
+
+  def myUnitFunc(): Unit = {
+    println("Use Unit for type of side effects")
+  }
+
+  val theUnit = () // this is the value that gets returned from type Unit, empty parenthesis 
 }
